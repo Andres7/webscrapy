2760ffe9cba3e2d92cd467e88fa1d5547ae4bf61
Andres7 <anesteban25@gmail.com>
2022-10-12 20:10:52 -0500

    ajustes en scroll y renderizado de pagina

--- services/scrapy.service.js
+++ services/scrapy.service.js
@@ -19,10 +19,11 @@ const service = {
             ]
         });
         const page = await brower.newPage();
+        await page.setViewport({ width: 1920, height: 1080 });
 
         let response1 = [];
         if (!mas) {
-            await page.goto(config.STOCK[0]);
+            await page.goto(config.STOCK[0], [1000, { waitUntil: "domcontentloaded" }]);
 
             await page.type('.header__search-form__flex-wrap input', texto)
             await page.click('.header__search-form__flex-wrap button');
@@ -47,7 +48,9 @@ const service = {
                 await page.click('.search button');
             }
             await page.waitForSelector('.wookmark-initialised');
-            await page.waitForTimeout(espera);
+            // await page.waitForTimeout(espera);
+
+            await autoScroll(page);
             response2 = await page.evaluate(() => {
                 const elements = document.querySelectorAll('.wookmark-initialised a');
                 const links = [];
@@ -77,11 +80,14 @@ const service = {
             ]
         });
         const page = await brower.newPage();
+        await page.setViewport({ width: 1920, height: 1080 });
 
         let response = [];
-        await page.goto(url);
+        await page.goto(url, [1000, { waitUntil: "domcontentloaded" }]);
         await page.waitForSelector('.wookmark-initialised');
-        await page.waitForTimeout(espera);
+        // await page.waitForTimeout(espera);
+
+        await autoScroll(page);
         response = await page.evaluate(() => {
             const elements = document.querySelectorAll('.wookmark-initialised a');
             const links = [];
@@ -97,5 +103,27 @@ const service = {
 
     }
 
+
+
 }
+
+const autoScroll = async (page) => {
+    await page.evaluate(async () => {
+        await new Promise((resolve) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if (totalHeight >= scrollHeight - window.innerHeight) {
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+}
+
 module.exports = service;
